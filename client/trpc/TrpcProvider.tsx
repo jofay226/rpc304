eaab504ce087d01d56db0f60d59421ca68584007
Javokhir Fayzullaev <jofay91@gmail.com>
2026-03-27 17:48:41 +0500

    27commit2

--- client/trpc/TrpcProvider.tsx
+++ client/trpc/TrpcProvider.tsx
@@ -1,7 +1,17 @@
 import { ReactNode } from "react";
+import { createTRPCReact } from "@trpc/react-query";
+import type { AppRouter } from "../server/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+
+export const trpc = createTRPCReact<AppRouter>();
 
 function TrpcProvider({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
 }
 
 export default TrpcProvider;
